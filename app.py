8aba13ba5517b99db2eb77b8b2fade0fd5010554
Raunak Chowdhuri <sauhaarda@gmail.com>
2023-05-09 20:04:23 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import oloren as olo
+
+@olo.register(description="Basic math operations on two numbers.")
+def operation(operation=olo.Choice(["Add", "Subtract", "Multiply", "Divide"]), a=olo.Num(), b=olo.Num()):
+    if operation == "Add":
+        return a + b
+    elif operation == "Subtract":
+        return a - b
+    elif operation == "Multiply":
+        return a * b
+    elif operation == "Divide":
+        return a / b
+      
+@olo.register()
+def error():
+    raise Exception("hello")
+    
+if __name__ == "__main__":
+    olo.run("operation")
